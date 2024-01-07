b19d1a4e80b4ed17dd24c99c5baf910e6345ac22
not-celeska <bushkov.mikhail@gmail.com>
2024-01-07 16:00:46 -0500

    did nrat. trig section finished until newer stuff or new ideas [BUG: NEED TO CHECK FOR <TRIANGLE INEQUALITY THEOREM>]

--- app/src/main/java/com/example/math10/NonRightTriangle.java
+++ app/src/main/java/com/example/math10/NonRightTriangle.java
@@ -11,57 +11,101 @@ import java.util.Arrays;
 
 public class NonRightTriangle extends AppCompatActivity {
 
-    TextView info;
+    TextView infon;
 
     EditText fieldSideA, fieldSideB, fieldSideC;
-    int SIDE_A = 0; int SIDE_B = 1; int SIDE_C = 2;
     EditText[] sideFields = new EditText[3];
+    int SIDE_A = 0; int SIDE_B = 1; int SIDE_C = 2;
     double[] sides = new double[3];
     int sidesInputted;
 
     EditText fieldAngleA, fieldAngleB, fieldAngleC;
-    EditText[] angleFields = new EditText[2];
-    int ANGLE_A = 0; int ANGLE_B = 1; int ANGLE_C  = 2;
-    double[] angles = new double[3]; // Angles [a, b, c]
+    EditText[] angleFields = new EditText[3];
+    int ANGLE_A = 0; int ANGLE_B = 1; int ANGLE_C = 2;
+    double[] angles = new double[3];
     int anglesInputted;
 
+    View resultsLayout; // = getLayoutInflater().inflate(R.layout.right_triangle_results, null);
 
-
+    TextView resultSideA; // = resultsLayout.findViewById(R.id.resultSideA);
+    TextView resultSideB;// = resultsLayout.findViewById(R.id.resultSideB);
+    TextView resultSideC; // = resultsLayout.findViewById(R.id.resultSideC);
+    TextView resultAngleA;// = resultsLayout.findViewById(R.id.resultAngleA);
+    TextView resultAngleB; // = resultsLayout.findViewById(R.id.resultAngleB);
+    TextView resultAngleC;
+    TextView resultPerimeter;
+    TextView resultArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_right_triangle);
 
-//        // Initialize Angle Fields.
-//        fieldAngleA = findViewById(R.id.angleAn);
-//        fieldAngleB = findViewById(R.id.angleBn);
-//        fieldAngleC = findViewById(R.id.angleCn);
-//        angleFields[ANGLE_A] = fieldAngleA;
-//        angleFields[ANGLE_B] = fieldAngleB;
-//        angleFields[ANGLE_C] = fieldAngleC;
-//
-//        // Initialize Side Fields.
-//        fieldSideA = findViewById(R.id.sideAn);
-//        fieldSideB = findViewById(R.id.sideBn);
-//        fieldSideC = findViewById(R.id.sideCn);
-//        sideFields[SIDE_A] = fieldSideA;
-//        sideFields[SIDE_B] = fieldSideB;
-//        sideFields[SIDE_C] = fieldSideC;
-//
-//        info = findViewById(R.id.infon);
-//        info.setText("Info will go here..");
-//
+        infon = findViewById(R.id.infon);
+
+        // Initialize Angle Fields.
+        fieldAngleA = findViewById(R.id.angleAn);
+        fieldAngleB = findViewById(R.id.angleBn);
+        fieldAngleC = findViewById(R.id.angleCn);
+        angleFields[ANGLE_A] = fieldAngleA;
+        angleFields[ANGLE_B] = fieldAngleB;
+        angleFields[ANGLE_C] = fieldAngleC;
+
+        // Initialize Side Fields.
+        fieldSideA = findViewById(R.id.sideAn);
+        fieldSideB = findViewById(R.id.sideBn);
+        fieldSideC = findViewById(R.id.sideCn);
+        sideFields[SIDE_A] = fieldSideA;
+        sideFields[SIDE_B] = fieldSideB;
+        sideFields[SIDE_C] = fieldSideC;
     }
 
-    public void sumbitData(View view) {
-
-        info.setText("Data submitted..\n");
+    public void submit(View view) {
         registerInput();
-        info.append(Arrays.toString(angles));
-        info.append("\n" + Arrays.toString(sides));
 
+        if (!(Arrays.equals(sides, new double[] {0.0, 0.0, 0.0}) && Arrays.equals(angles, new double[] {0.0, 0.0, 0.0}))) {
 
+            if (isValidInput()) {
+                infon.append("\nThats valid input!\n");
+
+                switch (howToCalculate()) {
+
+                    case 1:
+                        infon.append("Using 3 sides..\n");
+                        solveBySides();
+                        infon.append(Arrays.toString(angles));
+                        break;
+                    case 2:
+                        infon.append("Using 2 sides, 1 angle...\n");
+                        solveByTwoSidesOneAngle();
+                        infon.append(Arrays.toString(angles) + "\n");
+                        infon.append(Arrays.toString(sides));
+
+                        break;
+                    case 3:
+                        infon.append("Using 1 side 2 angles...\n");
+                        solveByOneSideTwoAngles();
+                        infon.append(Arrays.toString(angles) + "\n");
+                        infon.append(Arrays.toString(sides));
+                        break;
+                    case -1:
+                        infon.append("Not enough data..");
+                        break;
+
+                }
+
+                // display results in info TextView
+                infon.setText("ANGLES [A, B, C]: " + Arrays.toString(angles) + "\nSIDES [a, b, c]: " + Arrays.toString(sides));
+
+                // switch to the results layout
+
+                openResults(view);
+
+            }
+        }
+        else {
+            infon.setText("YOU DID NOT ENTER ANYTHING!");
+        }
     }
 
     public void registerInput() {
@@ -77,7 +121,6 @@ public class NonRightTriangle extends AppCompatActivity {
             if (!angle.isEmpty()) {
 
                 angles[angleField] = Double.parseDouble(angle);
-//                angleFields[angleField].getText().clear();
                 anglesInputted++;
             }
             else // if there was nothing entered.
@@ -103,10 +146,239 @@ public class NonRightTriangle extends AppCompatActivity {
         }
     }
 
+    public boolean isValidInput() {
+
+        // == ANGLES =============================================
+        if (anglesInputted == 3 && angles[ANGLE_A] + angles[ANGLE_B] + angles[ANGLE_C] != 180)
+        {
+            infon.setText("THE SUM OF ANGLES HAS TO BE EQUAL TO 180!");
+            return false;
+        }
+
+        return true;
+    }
+
+    public int howToCalculate() { // 1. 3 side way | 2. 2 side 1 angle | 3. 1 side 2 angles | -1. cant do anything
+        if (sidesInputted >= 3)
+        {
+            return 1;
+        }
+        else if ((sidesInputted >= 2) && (anglesInputted >= 1))
+        {
+            return 2;
+        }
+        else if ((sidesInputted >= 1) && (anglesInputted >= 2)) {
+            return 3;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public void solveBySides() {
+        angles[ANGLE_C] = roundTwoPlaces(Math.toDegrees(Math.acos((Math.pow(sides[SIDE_A], 2) + Math.pow(sides[SIDE_B], 2) - Math.pow(sides[SIDE_C], 2)) / (2 * sides[SIDE_A] * sides[SIDE_B]))));
+        angles[ANGLE_B] = roundTwoPlaces(Math.toDegrees(Math.acos((Math.pow(sides[SIDE_A], 2) + Math.pow(sides[SIDE_C], 2) - Math.pow(sides[SIDE_B], 2)) / (2 * sides[SIDE_A] * sides[SIDE_C]))));
+        angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_C] - angles[ANGLE_B]);
+    }
+
+    public void solveByOneSideTwoAngles() {
+        if (variableKnown('a')) {
+            if (variableKnown('A') && variableKnown('B')) {
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+            } else if (variableKnown('B') && variableKnown('C')) {
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_C] - angles[ANGLE_B]);
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+            } else { // ie a and c are known
+                angles[ANGLE_B] = roundTwoPlaces(180 - angles[ANGLE_C] - angles[ANGLE_A]);
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+            }
+        } else if (variableKnown('b')) {
+            if (variableKnown('A') && variableKnown('C')) {
+                angles[ANGLE_B] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            } else if (variableKnown('A') && variableKnown('B')) {
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            } else { // ie b and c are known
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_C] - angles[ANGLE_B]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            }
+        } else { // ie side c is known
+            if (variableKnown('A') && variableKnown('B')) {
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            } else if (variableKnown('A') && variableKnown('C')) {
+                angles[ANGLE_B] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            } else { // ie a and b are known
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            }
+        }
+    }
+
+    public void solveByTwoSidesOneAngle() {
+        if (variableKnown('A')) {
+            if (variableKnown('a') && variableKnown('b')) {
+                angles[ANGLE_B] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_A])) * sides[SIDE_B]) / sides[SIDE_A])));
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+            } else if (variableKnown('a') && variableKnown('c')) {
+                angles[ANGLE_C] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_A])) * sides[SIDE_C]) / sides[SIDE_A])));
+                angles[ANGLE_B] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_C]);
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_A] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_A])));
+            } else if (variableKnown('b') && variableKnown('c')) {
+                angles[ANGLE_A] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_B])) * sides[SIDE_C]) / sides[SIDE_B])));
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            }
+        } else if (variableKnown('B')) {
+            if (variableKnown('a') && variableKnown('b')) {
+                angles[ANGLE_A] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_B])) * sides[SIDE_A]) / sides[SIDE_B])));
+                angles[ANGLE_C] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_B]);
+                sides[SIDE_C] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_C]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            } else if (variableKnown('a') && variableKnown('c')) {
+                angles[ANGLE_C] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_B])) * sides[SIDE_C]) / sides[SIDE_B])));
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_B] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            } else if (variableKnown('b') && variableKnown('c')) {
+                angles[ANGLE_B] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_B])) * sides[SIDE_C]) / sides[SIDE_B])));
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_B] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_B] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_B])));
+            }
+        } else { // angle C known
+            if (variableKnown('a') && variableKnown('b')) {
+                angles[ANGLE_A] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_C])) * sides[SIDE_A]) / sides[SIDE_C])));
+                angles[ANGLE_B] = roundTwoPlaces(180 - angles[ANGLE_A] - angles[ANGLE_C]);
+                sides[SIDE_B] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_B]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            } else if (variableKnown('a') && variableKnown('c')) {
+                angles[ANGLE_B] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_C])) * sides[SIDE_B]) / sides[SIDE_C])));
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_B] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            } else if (variableKnown('b') && variableKnown('c')) {
+                angles[ANGLE_B] = roundTwoPlaces(Math.toDegrees(Math.asin((Math.sin(Math.toRadians(angles[ANGLE_C])) * sides[SIDE_B]) / sides[SIDE_C])));
+                angles[ANGLE_A] = roundTwoPlaces(180 - angles[ANGLE_B] - angles[ANGLE_C]);
+                sides[SIDE_A] = roundTwoPlaces((sides[SIDE_C] * Math.sin(Math.toRadians(angles[ANGLE_A]))) / Math.sin(Math.toRadians(angles[ANGLE_C])));
+            }
+        }
+    }
+
+    public boolean variableKnown(char variable)
+    {
+        switch (variable)
+        {
+            // SIDE LENGTHS
+            case 'a':
+                if (sides[SIDE_A] != 0.0)
+                {
+                    return true;
+                }
+                break;
+            case 'b':
+                if (sides[SIDE_B] != 0.0)
+                {
+                    return true;
+                }
+                break;
+            case 'c':
+                if (sides[SIDE_C] != 0.0)
+                {
+                    return true;
+                }
+                break;
+
+            // ANGLES
+            case 'A':
+                if (angles[ANGLE_A] != 0.0)
+                {
+                    return true;
+                }
+                break;
+            case 'B':
+                if (angles[ANGLE_B] != 0.0)
+                {
+                    return true;
+                }
+                break;
+            case 'C':
+                if (angles[ANGLE_C] != 0.0)
+                {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    public double roundTwoPlaces(double number) {
+        return (Math.round(100 * number) / 100.0);
+    }
+
+
+    public void openResults(View view) {
+        resultsLayout = getLayoutInflater().inflate(R.layout.non_right_triangle_results, null);
+
+        resultSideA = resultsLayout.findViewById(R.id.resultSideAn);
+        resultSideB = resultsLayout.findViewById(R.id.resultSideBn);
+        resultSideC = resultsLayout.findViewById(R.id.resultSideCn);
+        resultAngleA = resultsLayout.findViewById(R.id.resultAngleAn);
+        resultAngleB = resultsLayout.findViewById(R.id.resultAngleBn);
+        resultAngleC = resultsLayout.findViewById(R.id.resultAngleCn);
+        resultPerimeter = resultsLayout.findViewById(R.id.perimeterResultn);
+        resultArea = resultsLayout.findViewById(R.id.areaResultn);
+
+        // Use the TextViews as needed
+        resultSideA.setText(String.valueOf(sides[SIDE_A]));
+        resultSideB.setText(String.valueOf(sides[SIDE_B]));
+        resultSideC.setText(String.valueOf(sides[SIDE_C]));
+        resultAngleA.setText(String.valueOf(angles[ANGLE_A]));
+        resultAngleB.setText(String.valueOf(angles[ANGLE_B]));
+        resultAngleC.setText(String.valueOf(angles[ANGLE_C]));
+        resultPerimeter.setText("P = " + roundTwoPlaces(sides[SIDE_A] + sides[SIDE_B] + sides[SIDE_C]) + " units");
+        resultArea.setText("A = " + roundTwoPlaces(Math.sqrt(((sides[SIDE_A] + sides[SIDE_B] + sides[SIDE_C]) / 2) * ((sides[SIDE_A] + sides[SIDE_B] + sides[SIDE_C]) / 2 - sides[SIDE_A]) * ((sides[SIDE_A] + sides[SIDE_B] + sides[SIDE_C]) / 2 - sides[SIDE_B]) * ((sides[SIDE_A] + sides[SIDE_B] + sides[SIDE_C]) / 2 - sides[SIDE_C]))) + " units^2" );
+
+        // ... and so on
+
+        setContentView(resultsLayout);
+    }
+
+    public void closeResults(View view)
+    {
+        setContentView(R.layout.activity_non_right_triangle);
+
+        // Info feedback text area.
+        infon = findViewById(R.id.infon);
+
+        // Initialize Angle Fields.
+        fieldAngleA = findViewById(R.id.angleAn);
+        fieldAngleB = findViewById(R.id.angleBn);
+        fieldAngleC = findViewById(R.id.angleCn);
+        angleFields[ANGLE_A] = fieldAngleA;
+        angleFields[ANGLE_B] = fieldAngleB;
+        angleFields[ANGLE_C] = fieldAngleC;
+
+        // Initialize Side Fields.
+        fieldSideA = findViewById(R.id.sideAn);
+        fieldSideB = findViewById(R.id.sideBn);
+        fieldSideC = findViewById(R.id.sideCn);
+        sideFields[SIDE_A] = fieldSideA;
+        sideFields[SIDE_B] = fieldSideB;
+        sideFields[SIDE_C] = fieldSideC;
+    }
+
 
 
     public void close(View view) {
         finish();
     }
-
 }
